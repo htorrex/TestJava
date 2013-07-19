e72fafb89499e73e22b78214aff334c8deb35641
Hector Torres <torrhes@gmail.com>
2013-07-19 16:16:14 -0500

    5to commit con cambio en log

--- TestJava/src/main/java/mx/isystems/TestJava/App.java
+++ TestJava/src/main/java/mx/isystems/TestJava/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println( "Hello World!" );
         log.info("Prueba de modificacion de clase");
         log.info("Log 2 modificacion de archivo para probar cambios");
+        log.info("Que onda4");
     }
 }
