74dca6b4146f30c62d49d49e613df6e49afd9e74
Hector Torres <torrhes@gmail.com>
2013-07-19 15:32:39 -0500

    SECOND COMMIT

--- TestJava/src/main/java/mx/isystems/TestJava/App.java
+++ TestJava/src/main/java/mx/isystems/TestJava/App.java
@@ -1,13 +1,17 @@
 package mx.isystems.TestJava;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final Logger log = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        log.info("Prueba de modificacion de clase");
     }
 }
